e5382e23416ea3d14e49ee724dd580d15f1ab49e
Stephen Nowell <stephen@stardog.com>
2019-11-06 14:45:07 -0500

    Parameterize VM/NIC resources. Set trial email JVM argument

--- install-stardog.sh
+++ install-stardog.sh
@@ -5,3 +5,4 @@ sudo apt-get -y update
 sudo apt-get install -y stardog
 
 export STARDOG_HOME=/var/opt/stardog
+export STARDOG_SERVER_JAVA_ARGS="$STARDOG_SERVER_JAVA_ARGS -Dstardog.license.trial.auto.download.email=$1"
