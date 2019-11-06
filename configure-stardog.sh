7961996a7d7245b5269232a45f1c5dda9a71039a
Stephen Nowell <stephen@stardog.com>
2019-11-06 13:27:43 -0500

    Simplifying and cleaning up

--- configure-stardog.sh
+++
@@ -1,2 +0,0 @@
-curl http://packages.stardog.com/stardog.gpg.pub | sudo apt-key add
-echo "deb http://packages.stardog.com/deb/ stable main" | sudo tee -a /etc/apt/sources.list
--- install-stardog.sh
+++ install-stardog.sh
@@ -1,3 +1,6 @@
+curl http://packages.stardog.com/stardog.gpg.pub | sudo apt-key add
+echo "deb http://packages.stardog.com/deb/ stable main" | sudo tee -a /etc/apt/sources.list
+
 sudo apt-get -y update
 sudo apt-get install -y stardog
 
