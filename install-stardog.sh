44c54826143b35e9122dde19a4a06c412caebfc6
Stephen Nowell <stephen@stardog.com>
2019-11-07 16:00:34 -0500

    Add testing lic server. Still incompatible with deb stable main

--- install-stardog.sh
+++ install-stardog.sh
@@ -4,8 +4,6 @@ echo "deb http://packages.stardog.com/deb/ stable main" | sudo tee -a /etc/apt/s
 sudo apt-get -y update
 sudo apt-get install -y stardog
 
-echo "export STARDOG_SERVER_JAVA_ARGS='-Dstardog.license.trial.auto.download.email=$1 -Djava.library.path=/opt/stardog/lib'" | sudo tee -a /etc/stardog.env.sh
+echo "export STARDOG_SERVER_JAVA_ARGS='-Dstardog.license.trial.auto.download.email=$1 -Dstardog.license.server=https://reginaldtestingblue.stardog.com/lic'" | sudo tee -a /etc/stardog.env.sh
 
-if [ -n "$2" ]; then
-  sudo systemctl start stardog
-fi
+sudo systemctl start stardog
