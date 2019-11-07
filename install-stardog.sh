a274604bfca5319ab698ed08d5d65aea0b787e5c
Stephen Nowell <stephen@stardog.com>
2019-11-07 14:33:49 -0500

    Rearrange quotes

--- install-stardog.sh
+++ install-stardog.sh
@@ -4,6 +4,6 @@ echo "deb http://packages.stardog.com/deb/ stable main" | sudo tee -a /etc/apt/s
 sudo apt-get -y update
 sudo apt-get install -y stardog
 
-echo 'export STARDOG_SERVER_JAVA_ARGS="-Dstardog.license.trial.auto.download.email=$1"' | sudo tee -a /etc/stardog.env.sh
+echo "export STARDOG_SERVER_JAVA_ARGS='-Dstardog.license.trial.auto.download.email=$1'" | sudo tee -a /etc/stardog.env.sh
 
 sudo systemctl start stardog
