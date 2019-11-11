56eaa19369760f6595a4819b5d34134fb75a926b
Stephen Nowell <stephen@stardog.com>
2019-11-11 12:12:40 -0500

    Remove debug commands. Allow creator to specify new superuser id/passwd

--- install-stardog.sh
+++ install-stardog.sh
@@ -1,9 +1,23 @@
+EMAIL=$1
+ADMIN_USER=$2
+ADMIN_PASS=$3
+
 curl http://packages.stardog.com/stardog.gpg.pub | sudo apt-key add
 echo "deb http://packages.stardog.com/deb/ stable main" | sudo tee -a /etc/apt/sources.list
 
 sudo apt-get -y update
 sudo apt-get install -y stardog
 
-echo "export STARDOG_SERVER_JAVA_ARGS='-Dstardog.license.trial.auto.download.email=$1 -Dstardog.license.server=https://reginaldtestingblue.stardog.com/lic'" | sudo tee -a /etc/stardog.env.sh
+echo "export STARDOG_SERVER_JAVA_ARGS='-Dstardog.license.trial.auto.download.email=$EMAIL'" | sudo tee -a /etc/stardog.env.sh
 
 sudo systemctl start stardog
+sleep 20 # Gross
+
+if [ "$ADMIN_USER" = "admin" ]; then
+  if [ "$ADMIN_PASS" != "admin" ]; then
+    /opt/stardog/bin/stardog-admin user passwd --new-password "$ADMIN_PASS"
+  fi
+else
+  /opt/stardog/bin/stardog-admin user add --new-password "$ADMIN_PASS" --superuser "$ADMIN_USER"
+  /opt/stardog/bin/stardog-admin user remove --username "$ADMIN_USER" --passwd "$ADMIN_PASS" admin
+fi
